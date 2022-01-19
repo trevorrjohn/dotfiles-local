74ccd361c638368dba71b7e558f1741871425839
Trevor <673838+trevorrjohn@users.noreply.github.com>
2022-01-19 11:56:58 -0500

    Add init.lua for hammerspoon and shift-it
    https://github.com/peterklijn/hammerspoon-shiftit

---
+++ init.lua
@@ -0,0 +1,24 @@
+hs.loadSpoon("ShiftIt")
+-- spoon.ShiftIt:bindHotkeys({})
+local obj = {}
+obj.mash = { 'ctrl', 'alt', 'cmd', 'shift' }
+obj.mapping = {
+  left = { obj.mash, 'left' },
+  right = { obj.mash, 'right' },
+  up = { obj.mash, 'up' },
+  down = { obj.mash, 'down' },
+  upleft = { obj.mash, '1' },
+  upright = { obj.mash, '2' },
+  botleft = { obj.mash, '3' },
+  botright = { obj.mash, '4' },
+  maximum = { obj.mash, 'm' },
+  toggleFullScreen = { obj.mash, 'f' },
+  toggleZoom = { obj.mash, 'z' },
+  center = { obj.mash, 'c' },
+  nextScreen = { obj.mash, 'n' },
+  previousScreen = { obj.mash, 'p' },
+  resizeOut = { obj.mash, '=' },
+  resizeIn = { obj.mash, '-' }
+}
+
+spoon.ShiftIt:bindHotkeys(obj.mapping);
